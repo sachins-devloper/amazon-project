24e4cb4b05f503b00ab60d4490568eab974d7d92
sachins-devloper <sachinuce22@gmail.com>
2025-05-17 15:53:06 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,6 +80,13 @@ document.querySelectorAll(".js-add-to-cart-button").forEach((btn) => {
       });
     }
 
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
     console.log(cart);
   });
 });
