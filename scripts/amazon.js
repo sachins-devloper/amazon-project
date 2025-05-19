44175b38773b7523d8dff85ad36cd40b5999045e
sachins-devloper <sachinuce22@gmail.com>
2025-05-19 14:30:48 +0530

    cart quantity update from <select tag>

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -71,12 +71,19 @@ document.querySelectorAll(".js-add-to-cart-button").forEach((btn) => {
       }
     });
 
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+    // console.log(quantitySelector.value);
+
+    const quantity = Number(quantitySelector.value);
+
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
         productId,
-        quantity: 1,
+        quantity: quantity,
       });
     }
 
@@ -87,6 +94,6 @@ document.querySelectorAll(".js-add-to-cart-button").forEach((btn) => {
 
     document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 
-    console.log(cart);
+    // console.log(cart);
   });
 });
