65f3354b2c89a721817ad50d3ab6a372ec02b31b
sachins-devloper <sachinuce22@gmail.com>
2025-05-20 13:43:00 +0530

    order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,17 @@
-export const cart = [];
+export const cart = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 1,
+  },
+  {
+    id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
+    quantity: 2,
+  },
+  {
+    id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
+    quantity: 2,
+  },
+];
 
 export function addToCart(productId) {
   let matchingItem;
@@ -19,7 +32,7 @@ export function addToCart(productId) {
     matchingItem.quantity += quantity;
   } else {
     cart.push({
-      productId,
+      id: productId,
       quantity,
     });
   }
