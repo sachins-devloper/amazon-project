4f306e9eb1e8c352369871d9f191d1a075c98b30
sachins-devloper <sachinuce22@gmail.com>
2025-06-07 13:52:16 +0530

    Convert productss into classes

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,14 +1,14 @@
 class Cart {
   cartItems = undefined;
-  localStorageKey = undefined;
+  #localStorageKey = undefined;
 
   constructor(localStorageKey) {
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
   }
 
-  loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+  #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
     if (!this.cartItems) {
       this.cartItems = [
@@ -27,7 +27,7 @@ class Cart {
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addToCart(productId) {
@@ -114,6 +114,7 @@ class Cart {
 
 const cart = new Cart('cart-oop');
 const businessCart = new Cart('cart-business');
+
 
 console.log(cart);
 console.log(businessCart);
